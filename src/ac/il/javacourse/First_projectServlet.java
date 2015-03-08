2e3725990947c34beabbd40dd4dc9969779c6ab1
nicdu <nicoladur@gmail.com>
2015-03-08 17:25:20 +0200

    initial commit

---
+++ src/ac/il/javacourse/First_projectServlet.java
@@ -0,0 +1,11 @@
+package ac.il.javacourse;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class First_projectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
