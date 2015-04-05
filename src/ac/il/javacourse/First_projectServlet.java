883cbed5199806b391dbc54508f4f0b61b017946
nicdu <nicoladur@gmail.com>
2015-04-05 20:30:42 +0300

    Exercise 2

--- src/ac/il/javacourse/First_projectServlet.java
+++ src/ac/il/javacourse/First_projectServlet.java
@@ -5,7 +5,16 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class First_projectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
+		resp.setContentType("text/html");
 		resp.getWriter().println("Hello, world");
+		
+		int num1,num2,num3 ;
+		int result;
+		num1=4;
+		num2=3;
+		num3=7;
+		result=(num1+num2)*num3;
+		String resultStr = new String( "<h1>Result of ("+num1+"+"+num2+")*"+num3+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
